e92e509f1707a53f43933305fc92eae65d737a6a
weilai <weilai@nwbd.net>
2018-09-14 15:05:32 +0800

    1

--- src/main/java/com/space/utils/CalculateUtils.java
+++ src/main/java/com/space/utils/CalculateUtils.java
@@ -5,28 +5,31 @@ import java.text.DecimalFormat;
 
 /**
  * 计算工具类
+ *
  * @author zhuzhe
  * @date 2018/3/26 17:25
+ * @email zhe.zhu1@outlook.com
  */
 public class CalculateUtils {
 
     /**
      * 求两个数字的最大公约数
+     *
      * @param num1
      * @param num2
      * @return
      */
-    public static long commonDivisor(long num1,long num2){
+    public static long commonDivisor(long num1, long num2) {
         if (num1 == num2) {
             return num1;
         }
         long max = num1 > num2 ? num1 : num2;
         long min = num1 < num2 ? num1 : num2;
-        if ( max % min == 0 ) {
+        if (max % min == 0) {
             return min;
         }
-        for ( long i = min ; i > 0 ; i -- ) {
-            if ( min % i == 0 && max % i == 0) {
+        for (long i = min; i > 0; i--) {
+            if (min % i == 0 && max % i == 0) {
                 return i;
             }
         }
@@ -36,17 +39,17 @@ public class CalculateUtils {
     /**
      * 求两个数字的最小公倍数
      */
-    public static long diploid(long num1,long num2) {
+    public static long diploid(long num1, long num2) {
         if (num1 == num2) {
             return num1;
         }
         long max = num1 > num2 ? num1 : num2;
         long min = num1 < num2 ? num1 : num2;
-        if ( max % min == 0 ) {
+        if (max % min == 0) {
             return max;
         }
-        for ( long i = 1 ; i <= min ; i ++ ) {
-            if ( max * i % min == 0 ) {
+        for (long i = 1; i <= min; i++) {
+            if (max * i % min == 0) {
                 return max * i;
             }
         }
@@ -55,17 +58,18 @@ public class CalculateUtils {
 
     /**
      * 计算两个数字的最简整数比
+     *
      * @param num1
      * @param num2
-     * @return      num1:num2
+     * @return num1:num2
      */
-    public static String scale(Double num1, Double  num2){
+    public static String scale(Double num1, Double num2) {
         while (num1 != num1.intValue() || num2 != num2.intValue()) {
             num1 = mul(num1, 10);
             num2 = mul(num2, 10);
         }
-        for ( int i = num1.intValue(); i > 0 ; i-- ) {
-            if (num1.intValue()%i == 0 && num2.intValue()%i == 0) {
+        for (int i = num1.intValue(); i > 0; i--) {
+            if (num1.intValue() % i == 0 && num2.intValue() % i == 0) {
                 num1 = num1 / i;
                 num2 = num2 / i;
                 return num1.intValue() + ":" + num2.intValue();
@@ -76,6 +80,7 @@ public class CalculateUtils {
 
     /**
      * 精准乘法
+     *
      * @param v1
      * @param v2
      * @return
@@ -89,13 +94,14 @@ public class CalculateUtils {
     /**
      * 提供（相对）精确的除法运算。当发生除不尽的情况时，
      * 由scale参数指定精度，以后的数字四舍五入。
-     * @param v1 被除数
-     * @param v2  除数
-     * @param scale  表示表示需要精确到小数点以后几位
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 表示表示需要精确到小数点以后几位
      * @return 两个参数的商
      */
     public static double div(double v1, double v2, int scale) {
-        if(scale < 0) {
+        if (scale < 0) {
             throw new IllegalArgumentException("精确度不能小于0");
         }
         if (v2 == 0) {
@@ -103,29 +109,31 @@ public class CalculateUtils {
         }
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
         BigDecimal b2 = new BigDecimal(Double.toString(v2));
-        return b1.divide(b2,scale,BigDecimal.ROUND_HALF_UP).doubleValue();
+        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     /**
      * 提供精确的小数四舍五入处理
+     *
      * @param v     需要四舍五入的数字
      * @param scale 小数点后保留几位
-     * @return      四舍五入后的结果
+     * @return 四舍五入后的结果
      */
-    public static double round(double v,int scale) {
-        if(scale < 0 ) {
+    public static double round(double v, int scale) {
+        if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
         BigDecimal b = new BigDecimal(Double.toString(v));
         BigDecimal one = new BigDecimal("1");
-        return b.divide(one,scale,BigDecimal.ROUND_HALF_UP).doubleValue();
+        return b.divide(one, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     /**
      * 提供精确的减法运算
-     * @param v1    被减数
-     * @param v2    减数
-     * @return      两个参数的差
+     *
+     * @param v1 被减数
+     * @param v2 减数
+     * @return 两个参数的差
      */
     public static double sub(double v1, double v2) {
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
@@ -135,9 +143,10 @@ public class CalculateUtils {
 
     /**
      * 提供精确的加法运算
-     * @param v1    被加数
-     * @param v2    加数
-     * @return      两个参数的和
+     *
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
      */
     public static double add(double v1, double v2) {
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
@@ -148,6 +157,7 @@ public class CalculateUtils {
 
     /**
      * 提供精确加法计算的add方法，确认精确度
+     *
      * @param value1
      * @param value2
      * @param scale  小数点后保留几位
@@ -160,8 +170,8 @@ public class CalculateUtils {
     /**
      * 提供精确减法运算的sub方法，确认精确度
      *
-     * @param value1  被减数
-     * @param value2   减数
+     * @param value1 被减数
+     * @param value2 减数
      * @param scale  小数点后保留几位
      * @return 两个参数的求差之后，按精度四舍五入的结果
      */
@@ -171,6 +181,7 @@ public class CalculateUtils {
 
     /**
      * 提供精确乘法运算的mul方法，确认精确度
+     *
      * @param value1
      * @param value2
      * @param scale  小数点后保留几位
@@ -182,9 +193,10 @@ public class CalculateUtils {
 
     /**
      * 四舍五入格式化
-     * @param number 被格式化对象
-     * @param scale 保留小数点
-     * @param groupingSize  每组几个，0-代表不分组
+     *
+     * @param number       被格式化对象
+     * @param scale        保留小数点
+     * @param groupingSize 每组几个，0-代表不分组
      * @return 格式化数字字符串
      */
     public static String format(BigDecimal number, int scale, int groupingSize) {
@@ -196,6 +208,6 @@ public class CalculateUtils {
         }
         df.setMaximumFractionDigits(scale);
         df.setMinimumFractionDigits(scale);
-        return df.format(number.setScale(2,BigDecimal.ROUND_HALF_UP));
+        return df.format(number.setScale(2, BigDecimal.ROUND_HALF_UP));
     }
 }
